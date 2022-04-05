74e97eb57070cd173445fbcb65b5c067e5e93b43
smithylee81 <smithylee81@yahoo.co.uk>
2022-04-05 14:46:12 +0000

    prepared to deploy to heroku

---
+++ firstp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstpConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'firstp'
---
+++ firstp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
+
+# Create your views here.
+
+
+def get_firstp_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'firstp/firstp_list.html', context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_firstp_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }    
+    return render(request, 'firstp/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_firstp_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }   
+    return render(request, 'firstp/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_firstp_list')
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.delete()
+    return redirect('get_firstp_list')
